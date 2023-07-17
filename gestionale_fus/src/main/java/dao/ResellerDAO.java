a1a17a8483a030c20b5663a744cdd1bc022e207e
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-17 17:51:49 +0200

    Update ResellerDAO.java

--- gestionale_fus/src/main/java/dao/ResellerDAO.java
+++ gestionale_fus/src/main/java/dao/ResellerDAO.java
@@ -1,5 +1,13 @@
 package dao;
 
+import javax.persistence.EntityManager;
+
 public class ResellerDAO {
+
+	private final EntityManager entityManager;
+
+	public ResellerDAO(EntityManager _entityManager) {
+		this.entityManager = _entityManager;
+	}
 
 }
