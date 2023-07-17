cff8e33339f5f21efcd68bc7a4c05f8bf94b191d
EazyM93 <centini.manuel.93@gmail.com>
2023-07-17 14:09:20 +0200

    Copia del precedente progetto (ricostruito in java 17)

---
+++ gestionale_fus/src/main/java/app/GestionaleFus.java
@@ -0,0 +1,11 @@
+package app;
+
+public class GestionaleFus {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
---
+++ gestionale_fus/src/main/java/entities/Daily.java
@@ -0,0 +1,5 @@
+package entities;
+
+public class Daily extends TicketOffice{
+
+}
