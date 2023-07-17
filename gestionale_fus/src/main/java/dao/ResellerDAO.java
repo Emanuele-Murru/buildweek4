6988b5630baac19c60bc78e22a2ea916a7e8e489
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-17 18:39:32 +0200

    Update ResellerDAO.java

--- gestionale_fus/src/main/java/dao/ResellerDAO.java
+++ gestionale_fus/src/main/java/dao/ResellerDAO.java
@@ -1,11 +1,6 @@
 package dao;
 
-import java.time.LocalDate;
-
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
-import entities.Reseller;
 
 public class ResellerDAO {
 
@@ -16,16 +11,16 @@ public class ResellerDAO {
 	}
 
 	// - - - - - - - - - - - - - - - - - - - - getResellerTicketsByTime
-	public list<Ticket> getResellerTicketsByTime(Reseller _reseller, LocalDate _startDate, LocalDate _endDate) {
-
-		TypedQuery<Ticket> query = entityManager.createQuery(
-				"SELECT t FROM Ticket t WHERE t.reseller = :_reseller AND t.issueDate >= :_startDate AND t.issueDat <= :_endDate",
-				Ticket.class).setParameter("_reseller", _reseller).setParameter("_startDate", _startDate)
-				.setParameter("_endDate", _endDate);
-
-		List<Ticket> results = query.getResultList();
-
-		return results;
-	}
+//	public list<Ticket> getResellerTicketsByTime(Reseller _reseller, LocalDate _startDate, LocalDate _endDate) {
+//
+//		TypedQuery<Ticket> query = entityManager.createQuery(
+//				"SELECT t FROM Ticket t WHERE t.reseller = :_reseller AND t.issueDate >= :_startDate AND t.issueDat <= :_endDate",
+//				Ticket.class).setParameter("_reseller", _reseller).setParameter("_startDate", _startDate)
+//				.setParameter("_endDate", _endDate);
+//
+//		List<Ticket> results = query.getResultList();
+//
+//		return results;
+//	}
 
 }
