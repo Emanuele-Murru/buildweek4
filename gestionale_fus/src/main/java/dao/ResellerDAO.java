136c30a92aacf4e6d70d4d091300f98932438645
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-17 18:16:34 +0200

    Update ResellerDAO.java

--- gestionale_fus/src/main/java/dao/ResellerDAO.java
+++ gestionale_fus/src/main/java/dao/ResellerDAO.java
@@ -17,9 +17,15 @@ public class ResellerDAO {
 
 	// - - - - - - - - - - - - - - - - - - - - getResellerTicketsByTime
 	public list<Ticket> getResellerTicketsByTime(Reseller _reseller, LocalDate _startDate, LocalDate _endDate) {
+
 		TypedQuery<Ticket> query = entityManager.createQuery(
 				"SELECT t FROM Ticket t WHERE t.reseller = :_reseller AND t.issueDate >= :_startDate AND t.issueDat <= :_endDate",
-				Ticket.class).setParameter("_reseller", _reseller).setParameter("_startDate", _startDate).setParameter("_endDate", _endDate)
+				Ticket.class).setParameter("_reseller", _reseller).setParameter("_startDate", _startDate)
+				.setParameter("_endDate", _endDate);
+
+		List<Ticket> results = query.getResultList();
+
+		return results;
 	}
 
 }
