3bf15c8da529ded96c1d2ee166f838c9299a5a41
Manuel Centini <centini.manuel.93@gmail.com>
2023-07-18 11:11:27 +0200

    Update User.java

--- gestionale_fus/src/main/java/entities/User.java
+++ gestionale_fus/src/main/java/entities/User.java
@@ -7,8 +7,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
 	// - - - - - - - - - - - - - - - - - - - - attributes
@@ -28,56 +35,13 @@ public class User {
 	protected LocalDate birthDate;
 	protected String birthPlace;
 
-	// - - - - - - - - - - - - - - - - - - - - constructors
-	public User() {
-
-	}
-
 	public User(String _name, String _surname, LocalDate _birthDate, String _birthPlace) {
 		this.name = _name;
 		this.surname = _surname;
 		this.birthDate = _birthDate;
 		this.birthPlace = _birthPlace;
-
 	}
-
-	// - - - - - - - - - - - - - - - - - - - - getters & setters
-	public long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String _name) {
-		this.name = _name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String _surname) {
-		this.surname = _surname;
-	}
-
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(LocalDate _birthDate) {
-		this.birthDate = _birthDate;
-	}
-
-	public String getBirthPlace() {
-		return birthPlace;
-	}
-
-	public void setBirthPlace(String _birthPlace) {
-		this.birthPlace = _birthPlace;
-	}
-
+	
 	@Override
 	public String toString() {
 		return "User [" + name + ", " + surname + ", " + birthDate + ", " + birthPlace + "]";
