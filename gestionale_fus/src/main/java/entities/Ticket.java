15f2f8319c24fd82ff53f4ee27f1d765c215f636
EazyM93 <centini.manuel.93@gmail.com>
2023-07-19 11:13:41 +0200

    Update

--- gestionale_fus/src/main/java/entities/Ticket.java
+++ gestionale_fus/src/main/java/entities/Ticket.java
@@ -5,6 +5,10 @@ import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -12,7 +16,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "Ticket")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,7 +26,11 @@ public abstract class Ticket {
 	@Id
 	@GeneratedValue
 	private long id;
+	
 	private LocalDate issueDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "ticket_id")
 	private Reseller reseller;
 
 	public Ticket(LocalDate _issueDate, Reseller _reseller) {
